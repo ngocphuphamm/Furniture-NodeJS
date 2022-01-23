92da4a5862ae47a120cb432942dcea8bb942e54a
ngocphu <ngocphupham682001@gmail.com>
2022-01-23 10:27:47 +0700

    html

--- app/Controller/CategoriesController.js
+++ app/Controller/CategoriesController.js
@@ -8,11 +8,11 @@ class CategoriesController{
     
     var id = req.params.id; 
       // đặt biến phân trang 
-    var itemPerPage = 6; 
+    var itemsPerPage = 6; 
     // gán dữ liệu session id sản phẩm vào 
     req.session.categories = id ; 
     // tim các sản phẩm có loại dựa trên params id 
-    Product.find({"description.typeCode": id}, (err,dataP)=>
+    Product.find({"description.typeCode": id}, (err,dataProduct)=>
     {
       // tìm tất cả nhà sản xuất 
       Suppliers.find({},(err,suppliersData)=>
@@ -20,12 +20,12 @@ class CategoriesController{
           // tìm loại hiện sản phẩm hiện tại 
           Types.findOne({_id : id},(err,typeData)=>
           {
-              res.send({
-                data:dataP,
-                suppliersData : suppliersData, 
-                typeData : typeData ,
-                itemPerPage : itemPerPage , 
-                currentPage : 1 
+              res.render("categories/categories-list-item",{
+                suppliers: suppliersData,
+                products: dataProduct,
+                type: typeData,
+                itemsPerPage: itemsPerPage,
+                currentPage: 1
               })
           })
       })
