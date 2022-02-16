acbc03ff705f899cdedd92392e5d3dfcceebe326
ngocphu <ngocphupham682001@gmail.com>
2022-02-16 11:20:16 +0700

    dashboard

--- app/Controller/AdminController.js
+++ app/Controller/AdminController.js
@@ -1,11 +1,32 @@
 const Admin = require('../models/admin');
+const Product = require('../models/product');
+const Bills = require('../models/bill');
 class AdminController{
     getLogin(req,res,next){
         res.render("authencation/loginAdmin",{message: req.flash("Lỗi")});
     }
 
    getDashboardPage(req,res,next){
-     
+        if(req.isAuthenticated())
+        {
+            Product.find({},(err,dataProduct)=>{
+                Bills.find({},(err,dataBills)=>{
+                    // kiểm tra admin thêm lần nữa
+                    Admin.findOne({'loginInformation.userName':req.session.passport.user.username},(err,dataAdmin)=>{
+                        res.render('dashboards/dashboard',{
+                            message : req.flash("success"),
+                            customer :  dataAdmin , 
+                            abc : dataBills,
+                            products : dataProduct
+                        })
+                 
+                    })
+                })
+            })
+        }
+        else{
+            res.redirect('./admin/login');
+        }
    }
 }
 module.exports = new AdminController;
--- routes/admin.js
+++ routes/admin.js
@@ -8,6 +8,7 @@ const  adminController = require("../app/Controller/AdminController");
 
 route.get('/login',adminController.getLogin);
 // sử dụng passport  sử dụng  trong app.js nơi chứa server 
+// kiểm tra local admin-local xem có tài khoản đó không nếu thất bại thì chuyển qua  trang login 
 route.post('/login',passport.authenticate('admin-local',{failureRedirect:'/admin/login',successFlash:true,failureFlash:true})
                     ,adminController.getDashboardPage);
 
